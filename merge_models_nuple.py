8602d1e3de38ae76680c2cd78885528c9a07dfa8
vbuntinx <vincent.buntinx@epfl.ch>
2018-12-04 17:17:45 +0100

    adding the licence

--- merge_models_nuple.py
+++ merge_models_nuple.py
@@ -1,3 +1,26 @@
+#  Scanvan
+#
+#      Vincent Buntinx - vbuntinx@shogaku.ch
+#      Copyright (c) 2016-2018 DHLAB, EPFL
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+    #! \file   merge_models_nuple.py
+    #  \author Vincent Buntinx <vbuntinx@shogaku.ch>
+    #
+    #  Scanvan - https://github.com/ScanVan
+
 def fusion(model_1,model_2,nb_com):
     pos_1=model_1[0]
     rot_1=model_1[1]
--- spherical_algo_triplet.py
+++ spherical_algo_triplet.py
@@ -1,3 +1,26 @@
+#  Scanvan
+#
+#      Vincent Buntinx - vbuntinx@shogaku.ch
+#      Copyright (c) 2016-2018 DHLAB, EPFL
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+    #! \file   spherical_algo_triplet.py
+    #  \author Vincent Buntinx <vbuntinx@shogaku.ch>
+    #
+    #  Scanvan - https://github.com/ScanVan
+
 import numpy as np
 
 def pose_estimation(p3d_1,p3d_2,p3d_3,error_max):
